f0370e41b0ec68ab38d633022d1c2940d6da2cbc
Uche Omodu <chivoomodu@gmail.com>
2022-07-13 23:34:49 +0100

    Creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
-   <>
-   start
-   </>
+    <div>
+      <Router>
+        <Route path="/" element={<Home />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Router>
+    </div>
   );
 }
 
