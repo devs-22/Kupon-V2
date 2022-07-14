3c4c871bf74cab1aff446515c5ba530cbd5993c8
Uche Omodu <chivoomodu@gmail.com>
2022-07-14 14:44:54 +0100

    Building home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Header from '../../components/Header/Header';
+import './Home.scss';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <header>
+        <Header />
+      </header>
+      <section className="section-header">
+        <p className="section-header__p1">
+          Create detailed invoice in a{' '}
+          <span style={{ color: '#025CE4' }}>jiffy</span>.
+        </p>
+        <p className="section-header__p2">
+          Send invoices to your customers globally, Get to provide details of
+          your goods in seconds.
+        </p>
+      </section>
+      <section className="section-buttons">
+        <button className="section-buttons__createAccount">
+          Create Account
+        </button>
+        <button className="section-buttons__learnMore">Learn More</button>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
