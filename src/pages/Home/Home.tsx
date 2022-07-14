0608b0a53a9d81c7d99b369a4d02794d2e342760
Uche Omodu <chivoomodu@gmail.com>
2022-07-14 15:40:26 +0100

    Changing responsive view for tablets

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,22 +7,29 @@ const Home = () => {
       <header>
         <Header />
       </header>
-      <section className="section-header">
-        <p className="section-header__p1">
-          Create detailed invoice in a{' '}
-          <span style={{ color: '#025CE4' }}>jiffy</span>.
-        </p>
-        <p className="section-header__p2">
-          Send invoices to your customers globally, Get to provide details of
-          your goods in seconds.
-        </p>
-      </section>
-      <section className="section-buttons">
-        <button className="section-buttons__createAccount">
-          Create Account
-        </button>
-        <button className="section-buttons__learnMore">Learn More</button>
-      </section>
+      <div className="flex-head">
+        <div className="flex-head__1">
+          <section className="section-header">
+            <p className="section-header__p1">
+              Create detailed invoice in a{' '}
+              <span style={{ color: '#025CE4' }}>jiffy</span>.
+            </p>
+            <p className="section-header__p2">
+              Send invoices to your customers globally, Get to provide details
+              of your goods in seconds.
+            </p>
+          </section>
+          <section className="section-buttons">
+            <button className="section-buttons__createAccount">
+              Create Account
+            </button>
+            <button className="section-buttons__learnMore">Learn More</button>
+          </section>
+        </div>
+        <div className="flex-head__2">
+          <p>11</p>
+        </div>
+      </div>
     </>
   );
 };
