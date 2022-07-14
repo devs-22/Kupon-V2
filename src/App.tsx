3399626d333b8faf34de16b2d190751ac7840450
Uche Omodu <chivoomodu@gmail.com>
2022-07-14 14:44:43 +0100

    adding my routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
@@ -8,9 +7,11 @@ function App() {
   return (
     <div>
       <Router>
-        <Route path="/" element={<Home />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/SignIn" element={<SignIn />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/SignIn" element={<SignIn />} />
+        </Routes>
       </Router>
     </div>
   );
