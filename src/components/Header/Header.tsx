8532033b68f042d3becf991a3bfde8a6e958758f
Uche Omodu <chivoomodu@gmail.com>
2022-07-14 14:45:30 +0100

    building header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
+import { MenuToggle } from './menuToggle';
+
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+  const toggled = () => {
+    setOpen(!isOpen);
+  };
+  return (
+    <nav>
+      <NavLink to={'/'} className="header-navLink">
+        Kupon
+      </NavLink>
+      <div className="header-btn">
+        <button className="header-btn__signUp">Sign Up</button>
+        <button className="header-btn__signIn">Sign In</button>
+        <MenuToggle isOpen={isOpen} toggle={toggled} />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
