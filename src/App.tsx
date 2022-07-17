a66ac1406b99300ec7f033015b91b4330d49bb19
Uche Omodu <79480742+qgatssdev@users.noreply.github.com>
2022-07-17 22:29:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
-
+//
 function App() {
   return (
     <div>
