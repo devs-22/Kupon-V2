44981cae7b130f04bd246f449844d031d0dc94da
Uche Omodu <chivoomodu@gmail.com>
2022-07-21 23:33:13 +0100

    building Step 1

---
+++ src/pages/SignUp/Step1/Step1.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import { InputAdornment } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/bootstrap.css';
+import OutlinedInput from '@mui/material/OutlinedInput';
+// import Visibility from '@mui/icons-material/Visibility';
+// import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+
+const Step1 = () => {
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false,
+  });
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <div>
+        <label>Full Name</label>
+        <TextField id="outlined-basic" variant="outlined" />
+      </div>
+      <div>
+        <label>Email</label>
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <img
+                  src={require('../../../assets/Tick Square.svg').default}
+                  alt="tick"
+                />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+      <div>
+        <label>Phone</label>
+        <PhoneInput country={'ng'} preferredCountries={['us', 'uk', 'gh']} />
+      </div>
+      <div>
+        <label>Password</label>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {/* {values.showPassword ? <VisibilityOff /> : <Visibility />} */}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Step1;
