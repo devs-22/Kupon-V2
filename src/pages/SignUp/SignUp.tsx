63210094763fed345951de9a030a7dbe221403c3
Uche Omodu <chivoomodu@gmail.com>
2022-07-21 23:33:45 +0100

    Building Sign Up

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './SignUp.scss';
+import Step1 from './Step1/Step1';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="signUp-root">
+      <section className="signUp-root__1">
+        <p>Kupon</p>
+        <p>Create Account</p>
+        <p>Welcome, please create your account in 2 easy steps 🤩</p>
+        <form>
+          <Step1 />
+        </form>
+      </section>
+      <section className="signUp-root__2">images</section>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
