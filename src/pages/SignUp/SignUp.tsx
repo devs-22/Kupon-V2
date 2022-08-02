bf8e38f44f2892fedfb6c65a222aedd27d57e6d5
Uche Omodu <chivoomodu@gmail.com>
2022-08-02 20:20:43 +0100

    Styling Sign up for mobile

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -6,6 +6,11 @@ const SignUp = () => {
   return (
     <div className="signUp-root">
       <section className="signUp-root__1">
+        <img
+          src={require('../../assets/Polypodium 2.svg').default}
+          alt="logo"
+          className="or-l"
+        />
         <p className="signUp-root__1__p1">Kupon</p>
         <p className="signUp-root__1__p2">Create Account</p>
         <form>
