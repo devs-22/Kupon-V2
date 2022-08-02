8c6ec543ebfcd478c233cc06fc53f71c92b46219
Uche Omodu <chivoomodu@gmail.com>
2022-08-02 23:48:47 +0100

    building step 2

--- src/pages/SignUp/Step2/Step2.tsx
+++ src/pages/SignUp/Step2/Step2.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './Step2.scss';
+import TextField from '@mui/material/TextField';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-const Step2 = () => {
+const Step2 = ({ setClicked }: any) => {
   return (
-    <div>Step2</div>
-  )
-}
+    <div className="step2__root">
+      <p className="step2__root__p1">
+        Congrats on signing up, now fill in your business information 😁
+      </p>
+      <ArrowBackIcon
+        onClick={() => {
+          setClicked(false);
+        }}
+        className="step2__root__arrowBack"
+      />
+      <div>
+        <label>Business Logo</label>
+      </div>
+      <div className="step2__labels">
+        <label>Business Name</label>
+        <TextField variant="outlined" className="label_step2" />
+      </div>
+      <div className="step2__labels">
+        <label>Business Address</label>
+        <TextField variant="outlined" className="label_step2" />
+      </div>
+      <button className="step2__submit">Done</button>
+    </div>
+  );
+};
 
-export default Step2
+export default Step2;
