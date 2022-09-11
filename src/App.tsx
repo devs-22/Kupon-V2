5dbfaf3bba17135ffc16e0d8e767700c5f46fcc7
Uche Omodu <chivoomodu@gmail.com>
2022-09-11 22:06:03 +0100

    adjusting routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import './App.css';
+import LearnMore from './pages/LearnMore/LearnMore';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/LearnMore" element={<LearnMore />} />
         </Routes>
       </Router>
     </div>
