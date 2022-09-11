df0cd6c14bab729649a8c64e77b109a9bc9a2d3b
Uche Omodu <chivoomodu@gmail.com>
2022-09-11 22:05:34 +0100

    building Learn More page

--- src/pages/LearnMore/LearnMore.tsx
+++ src/pages/LearnMore/LearnMore.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import './LearnMore.scss';
 const LearnMore = () => {
   return (
-    <div>LearnMore</div>
-  )
-}
+    <div className="learnMore__root">
+      <header>
+        <Header />
+      </header>
+      <div className="learnMore__getToKnowUs">
+        <img src={require('../../assets/Polypodium.svg').default} alt="" />
+        <p>Get to know us</p>
+        <img src={require('../../assets/Polypodium.svg').default} alt="" />
+        <img
+          src={require('../../assets/Point_right.svg').default}
+          alt=""
+          className="hand"
+        />
+      </div>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam
+          deserunt explicabo earum magnam sed, delectus dolores possimus
+          doloremque voluptatibus sunt ut obcaecati corporis praesentium ullam?
+          Cupiditate rerum, tenetur obcaecati ducimus laudantium odit
+          exercitationem nihil eius dignissimos eligendi? Error, voluptatibus
+          mollitia?
+        </p>
+      </section>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
 
-export default LearnMore
+export default LearnMore;
