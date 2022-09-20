d008c64acc28d17a907e97f913400f1ca7b5d1f6
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-20 16:54:49 +0100

    added header and footer

--- src/pages/FAQ/FAQ.tsx
+++ src/pages/FAQ/FAQ.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 const FAQ = () => {
   return (
-    <div>FAQ</div>
+    <div className="FAQ__root">
+      <header>
+        <Header />
+      </header>
+      <div className="FAQ__topic">
+        <img src={require('../../assets/Polypodium.svg').default} alt="" className='hand'/>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
