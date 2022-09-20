0963c1f05caad1d8f344901aded60b26774a3a2d
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-20 18:17:01 +0100

    section fix

--- src/pages/FAQ/FAQ.tsx
+++ src/pages/FAQ/FAQ.tsx
@@ -1,15 +1,58 @@
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import './FAQ.scss';
+import Faq from "react-faq-component";
 
 const FAQ = () => {
+  const data = {
+    rows: [
+        {
+            title: "What is Kupon?",
+            content: `Kupon makes it easy for small business owners to create invoices for their customers`,
+        },
+        {
+            title: "Is there a limit to the amount of Kupon I can create?",
+            content:
+                "Kupon makes it easy for small business owners to create invoices for their customers",
+        },
+        {
+            title: "What should I do if I forget my password?",
+            content: `Kupon makes it easy for small business owners to create invoices for their customers`,
+        },
+        {
+            title: "How do I change my registered email on Kupon?",
+            content: `Kupon makes it easy for small business owners to create invoices for their customers`,
+        },
+    ],
+};
+
+const styles = {
+    titleTextColor: "black",
+    rowTitleColor: "black",
+};
+
+const config = {
+    animate: true,
+    tabFocus: true
+};
+
   return (
     <div className="FAQ__root">
       <header>
         <Header />
       </header>
       <div className="FAQ__topic">
-        <img src={require('../../assets/Polypodium.svg').default} alt="" className='hand'/>
+        <img src={require('../../assets/Polypodium.svg').default} alt=""/>
+        <p>FAQs</p>
+        <img src={require('../../assets/Polypodium.svg').default}alt="" />
       </div>
+      <section>
+      <Faq
+                data={data}
+                styles={styles}
+                config={config}
+            />
+      </section>
       <footer>
         <Footer />
       </footer>
