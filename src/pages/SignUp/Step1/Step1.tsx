7faf222f508486cc7763501cdcfd7594eade61d1
Uche Omodu <chivoomodu@gmail.com>
2022-10-01 21:14:30 +0100

    building step1

--- src/pages/SignUp/Step1/Step1.tsx
+++ src/pages/SignUp/Step1/Step1.tsx
@@ -27,96 +27,100 @@ const Step1 = ({ setClicked }: any) => {
   };
 
   return (
-    <div className="step1__root">
+    <>
       <p className="step1__root_p1">
         Welcome, please create your account in 2 easy steps 🤩
       </p>
-      <div className="step1__labels">
-        <label>Full Name</label>
-        <TextField className="labels__textField" variant="outlined" />
+      <div className="step1__root">
+        <div className="step1__root__labels">
+          <div className="step1__labels">
+            <label>Full Name</label>
+            <TextField className="labels__textField" variant="outlined" />
+          </div>
+          <div className="step1__labels">
+            <label>Email</label>
+            <TextField
+              className="labels__textField"
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <img
+                      src={require('../../../assets/Tick Square.svg').default}
+                      alt="tick"
+                    />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+          <div className="step1__labels">
+            <label>Phone</label>
+            <PhoneInput
+              country={'ng'}
+              preferredCountries={['us', 'uk', 'gh']}
+              inputClass={'number-class'}
+            />
+          </div>
+          <div className="step1__labels">
+            <label>Password</label>
+            <OutlinedInput
+              className="labels__textField"
+              type={values.showPassword ? 'text' : 'password'}
+              // value={values.password}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? (
+                      <VisibilityOffIcon />
+                    ) : (
+                      <VisibilityIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </div>
+          <div className="step1__labels">
+            <label>Confirm Password</label>
+            <OutlinedInput
+              className="labels__textField"
+              type={values.showPassword ? 'text' : 'password'}
+              // value={values.password}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? (
+                      <VisibilityOffIcon />
+                    ) : (
+                      <VisibilityIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </div>
+          <button
+            className="step-1__button"
+            onClick={() => {
+              setClicked(true);
+            }}
+          >
+            Step 1/2
+          </button>
+        </div>
       </div>
-      <div className="step1__labels">
-        <label>Email</label>
-        <TextField
-          className="labels__textField"
-          variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <img
-                  src={require('../../../assets/Tick Square.svg').default}
-                  alt="tick"
-                />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </div>
-      <div className="step1__labels">
-        <label>Phone</label>
-        <PhoneInput
-          country={'ng'}
-          preferredCountries={['us', 'uk', 'gh']}
-          inputClass={'number-class'}
-        />
-      </div>
-      <div className="step1__labels">
-        <label>Password</label>
-        <OutlinedInput
-          className="labels__textField"
-          type={values.showPassword ? 'text' : 'password'}
-          // value={values.password}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {values.showPassword ? (
-                  <VisibilityOffIcon />
-                ) : (
-                  <VisibilityIcon />
-                )}
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-      </div>
-      <div className="step1__labels">
-        <label>Confirm Password</label>
-        <OutlinedInput
-          className="labels__textField"
-          type={values.showPassword ? 'text' : 'password'}
-          // value={values.password}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {values.showPassword ? (
-                  <VisibilityOffIcon />
-                ) : (
-                  <VisibilityIcon />
-                )}
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-      </div>
-      <button
-        className="step-1__button"
-        onClick={() => {
-          setClicked(true);
-        }}
-      >
-        Step 1/2
-      </button>
-    </div>
+    </>
   );
 };
 
